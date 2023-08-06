e946926200ce939ded508ce48ebc8d926b3a3504
Beginner0x1 <138056577+Beginner0x1@users.noreply.github.com>
2023-08-06 10:26:52 -0400

    Update Module_3_Challenge_2.sol

--- Module_3_Challenge_2.sol
+++ Module_3_Challenge_2.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+// Abstract Contract
+abstract contract Animal {
+    string public species;
+
+    // Abstract function
+    function makeSound() external virtual;
+
+    // Concrete function
+    function setSpecies(string memory _species) external {
+        species = _species;
+    }
+}
+
+// Interface
+interface Flying {
+    function fly() external;
+}
+
+// Smart Contract that implements both the abstract contract and the interface
+contract Bird is Animal, Flying {
+    string public name;
+
+    constructor(string memory _name) {
+        name = _name;
+        species = "Bird";
+    }
+
+    // Implementation of the abstract function from the Animal abstract contract
+    function makeSound() external override {
+        // Implement the sound a bird makes here
+    }
+
+    // Implementation of the function from the Flying interface
+    function fly() external override {
+        // Implement the flying behavior of the bird here
+    }
+}
